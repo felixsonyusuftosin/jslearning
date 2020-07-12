03f7e158c05617d340b8c15e9bc2045dad453e54
Fidel <fidelcollin@icloud.com>
2020-07-12 13:54:36 +0100

    implement page

--- page.js
+++ page.js
@@ -1,3 +1,13 @@
  /*
  * @file pagination class should go here 
  */
+ class Pages{
+  constructor(pages){
+    this.pages = pages;
+  }
+  next(){
+    if(this.pages.length === 0){
+return;
+    }
+  }
+}
