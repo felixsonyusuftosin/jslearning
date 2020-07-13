88ae22582db66aea43940705d5aaaa88482b228b
Fidel Collin <fidelcollin@icloud.com>
2020-07-13 15:44:52 +0100

    implement next

--- page.js
+++ page.js
@@ -1,13 +1,32 @@
- /*
- * @file pagination class should go here 
+/*
+ * @file pagination class should go here
  */
- class Pages{
-  constructor(pages){
+class Pages {
+  constructor(pages, current = 0, limit = 2) {
     this.pages = pages;
+    this.current = current;
+    this.limit = limit;
   }
-  next(){
-    if(this.pages.length === 0){
-return;
+  next() {
+    if (this.current > this.pages.length - 1 || this.pages.length === 0) {
+      console.log("exhausted");
+      return;
     }
+    const result = this.pages.slice(this.current, this.current + this.limit);
+    this.current = this.current + this.limit;
+    return result;
   }
 }
+
+const pageList = new Pages(["a", 1, 2, 3, 4, 5, 6, 7]);
+
+console.log(pageList.next());
+console.log(pageList.next());
+console.log(pageList.next());
+console.log(pageList.next());
+console.log(pageList.next());
+console.log(pageList.next());
+console.log(pageList.next());
+console.log(pageList.next());
+console.log(pageList.next());
+console.log(pageList.next());
