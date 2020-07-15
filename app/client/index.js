a2e120cbeadb52172b7c31a730efa4b807536f87
Fidel Collin <fidelcollin@icloud.com>
2020-07-15 20:54:26 +0100

    implementing sign up form

---
+++ app/client/index.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  const form = document.getElementById("form");
+  const firstName = document.getElementById("fname");
+  const lastName = document.getElementById("lname");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+
+  form.addEventListener("submit", (e) => {});
+};
