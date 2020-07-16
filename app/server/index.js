3d2c5380c819e948efbdfcb0c58c9aa6ec2e52ae
ftosin@botkeeper.com <ftosin@botkeeper.com>
2020-07-16 21:52:02 +0100

    add server code

---
+++ app/server/index.js
@@ -0,0 +1,20 @@
+const app = require('express')()
+
+app.post('/user', (req, res) => {
+  console.log(req)
+  const body = req.body
+  res.send(body)
+})
+
+app.get('/', (req, res) => {
+  res.send(' Some random text that i just thought of in fact <h1> Some random</h1>')
+})
+
+app.get('/user', (req, res) => {
+  res.send(' Hey welcome here')
+})
+
+app.listen(9898, 'localhost', () => {
+  console.log('app is listening on port 9898 ... ')
+
+})
